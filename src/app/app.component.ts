8b5f8a8701b58b34253e53bd83ba34f724d051b9
Álvaro Braz <alvarobraz83@gmail.com>
2023-11-15 20:00:41 -0300

    conhecendo ngOnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,51 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
     <!-- <app-title title="Olá Desenvolvedor!"></app-title> -->
-    {{valor}}
-    <button (click)="adicionar()">Adicionar</button>
+    <!-- {{valor}}
+    <button (click)="adicionar()">Adicionar</button> -->
+    <app-title *ngIf="destruir"></app-title>
+    <br>
+    <button (click)="destruirComponent()">{{buttonTitle}}</button>
     <router-outlet></router-outlet>
   `
 })
-export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
 
-  public valor: number = 1;
+  // public valor: number = 1;
+
+  public buttonTitle: string =  ""
+
+  public destruir: boolean = true;
 
   constructor() {
   }
 
-  public adicionar(): number {
-    return this.valor +=1
+  // public adicionar(): number {
+  //   return this.valor +=1
+  // }
+
+  ngOnInit(): void {
+    this.buttonTitle = "Ocultar componente"
   }
 
-  ngOnInit(): void {}
+  public destruirComponent() {
+    this.destruir = !this.destruir
+  }
 
   // Implementação de DoCheck
   ngDoCheck(): void  {
     console.log('ngDoCheck executado');
+    console.log(this.destruir)
+    if(this.destruir === false) {
+      this.buttonTitle = "Revelar componente"
+    }
+    else {
+      this.buttonTitle = "Ocultar componente"
+    }
   }
 
   // Implementação de AfterContentInit
@@ -46,6 +66,11 @@ export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterCon
   // Implementação de AfterViewChecked
   ngAfterViewChecked(): void  {
     console.log('ngAfterViewChecked executado');
+  }
+
+  // Implementação de ngOnDestroy
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy executado');
   }
 
 }
