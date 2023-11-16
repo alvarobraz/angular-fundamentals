58c1e5749f50aae4c168469bf1239d3ce569aba0
Álvaro Braz <alvarobraz83@gmail.com>
2023-11-16 01:00:32 -0300

    @Input()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,17 +14,26 @@ import { Component, OnInit } from '@angular/core';
       <h3>Final da aula 2</h3>
     </app-diretivas-atributos> -->
     <!-- <app-diretivas-atributos></app-diretivas-atributos> -->
-    <app-new-component></app-new-component>
+    <!-- <app-new-component></app-new-component> -->
+
+    <app-input [contador]="valueContador"></app-input>
+    <button (click)="increment()">Incrementar contador</button>
     <router-outlet></router-outlet>
   `
 })
 export class AppComponent implements OnInit  {
+
+  public valueContador: number = 0
 
   constructor() {
   }
 
 
   ngOnInit(): void {
+  }
+
+  public increment() {
+    this.valueContador += 1
   }
 
 }
