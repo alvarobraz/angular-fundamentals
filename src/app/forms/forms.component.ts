1273c6b1fa1fc39ae4edb4426775f76ad6a81b25
Álvaro Braz <alvarobraz83@gmail.com>
2023-11-16 15:37:13 -0300

    select ngFor e ngValue

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsComponent implements OnInit {
 
+  public ListComidas: Array<{ comida: string, preco: string }> = [
+    { comida: 'X-Salada', preco: '10.00' },
+    { comida: 'X-Frango', preco: '15.00'  },
+    { comida: 'X-Tudo', preco: '20.00'  },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
