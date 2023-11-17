c88b924131589e33a6e5672200c750c569bd5fdc
Álvaro Braz <alvarobraz83@gmail.com>
2023-11-17 09:23:40 -0300

    criando rotas e rota coringa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 // Components
-import { ReactiveformsComponent } from './reactiveforms/reactiveforms.component';
+// import { ReactiveformsComponent } from './reactiveforms/reactiveforms.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
 
-const routes: Routes = [{
-  path: '',
-  component: ReactiveformsComponent
-}];
+// const routes: Routes = [{
+//   path: '',
+//   component: ReactiveformsComponent
+// }];
+
+const routes: Routes = [
+  { path : '', component: HomeComponent, pathMatch: 'full' },
+  { path : 'sobre', component: SobreComponent },
+  { path : '404', component: PageErrorComponent },
+  { path : '**', redirectTo: '404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
+
+// Others
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { DiretivasEstruturaisComponent } from './diretivas-estruturais/diretivas-estruturais.component';
 import { DiretivasAtributosComponent } from './diretivas-atributos/diretivas-atributos.component';
 import { SharedModule } from './shared/shared.module';
 import { FormsComponent } from './forms/forms.component';
 import { ReactiveformsComponent } from './reactiveforms/reactiveforms.component';
+
+
+//Pages
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
 
 
 @NgModule({
@@ -22,7 +30,10 @@ import { ReactiveformsComponent } from './reactiveforms/reactiveforms.component'
     DiretivasEstruturaisComponent,
     DiretivasAtributosComponent,
     FormsComponent,
-    ReactiveformsComponent
+    ReactiveformsComponent,
+    HomeComponent,
+    SobreComponent,
+    PageErrorComponent
   ],
   imports: [
     BrowserModule,
